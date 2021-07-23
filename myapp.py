409665788ea8e8b7c082f3799d1b3a9374903f79
rehanunus <rehanunus@gmail.com>
2021-07-23 17:12:05 +0530

    first commit

---
+++ myapp.py
@@ -0,0 +1,13 @@
+import requests
+import json
+URL="http://127.0.0.1:8000/stucreate/"
+data={
+    'name':'Ajit',
+    'roll':102,
+    'city':'Bhagalpur'
+}
+
+json_data=json.dumps(data)
+r=requests.post(url=URL,data=json_data)
+data=r.json()
+print(data)
